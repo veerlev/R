5b5332eb29d3bb3fcc7701a7c1b303345feedc1f
veerlev <47982314+veerlev@users.noreply.github.com>
2019-09-12 16:52:00 +0100

    Update hXMooc.R

--- hXMooc.R
+++ hXMooc.R
@@ -604,4 +604,11 @@ abbs=c("MA","ME","MI","MO","MU")
 
 # Use the %in% command to check if the entries of abbs are abbreviations in the the murders data frame
 abbs %in% murders$abb
+# Store the 5 abbreviations in abbs. (remember that they are character vectors)
+abbs <- c("MA", "ME", "MI", "MO", "MU") 
 
+# Use the `which` command and `!` operator to find out which index abbreviations are not actually part of the dataset and store in `ind`
+ind <- which(! abbs %in% murders$abb)
+ind
+# Names of abbreviations in `ind`
+abbs[ind]
