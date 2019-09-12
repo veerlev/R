d88130429ebcaeaadbfda1cabe69b9e96ed155ae
veerlev <47982314+veerlev@users.noreply.github.com>
2019-09-12 16:32:46 +0100

    Scripts from the HarvardX MOOC

---
+++ hXMooc.R
@@ -0,0 +1,607 @@
+# Here is how you compute the sum for the first 20 integers
+20*(20+1)/2 
+
+# However, we can define a variable to use the formula for other values of n
+n <- 20
+n*(n+1)/2
+
+n <- 25
+n*(n+1)/2
+
+# Below, write code to calculate the sum of the first 100 integers
+n <- 100
+n*(n+1)/2
+
+# Below, write code to calculate the sum of the first 1000 integers 
+n<-1000
+n*(n+1)/2
+
+# log to the base 2 
+log2(16)
+
+# sqrt of the log to the base 2 of 16:
+sqrt(log2(16))
+
+# Compute log to the base 10 (log10) of the sqrt of 100. Do not use variables.
+log10(sqrt(100))
+
+# Load package and data
+
+library(dslabs)
+data(murders)
+
+# Use the function names to extract the variable names 
+names(murders)
+# To access the population variable from the murders dataset use this code:
+p <- murders$population 
+
+# To determine the class of object `p` we use this code:
+class(p)
+
+# Use the accessor to extract state abbreviations and assign it to a
+a<-murders$abb
+# Determine the class of a
+class(a)
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]] 
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+b <- murders[["abb"]]
+# Check if `a` and `b` are identical 
+identical(a, b)
+# We can see the class of the region variable using class
+class(murders$region)
+
+# Determine the number of regions included in this variable 
+length(levels(murders$region))
+# Here is an example of what the table function does
+x <- c("a", "a", "b", "b", "b", "c")
+table(x)
+
+# Write one line of code to show the number of states per region
+table(murders$region)
+
+# Here is an example creating a numeric vector named cost
+cost <- c(50, 75, 90, 100, 150)
+
+# Create a numeric vector to store the temperatures listed in the instructions into a vector named temp
+# Make sure to follow the same order in the instructions
+temp <- c(Beijing=35, Lagos= 88, Paris= 42, "Rio de Janeiro"= 84, "San Juan"= 81, Toronto=30)
+temp
+
+# here is an example of how to create a character vector
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")
+
+# Create a character vector called city to store the city names
+# Make sure to follow the same order as in the instructions
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Associate the cost values with its corresponding food item
+cost <- c(50, 75, 90, 100, 150)
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")
+names(cost) <- food
+
+# You already wrote this code
+temp <- c(35, 88, 42, 84, 81, 30)
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Associate the temperature values with its corresponding city
+names(temp) <-city
+
+# cost of the last 3 items in our food list:
+cost[3:5]
+
+# temperatures of the first three cities in the list:
+temp[1:3]
+
+# Access the cost of pizza and pasta from our food list 
+cost[c(1,5)]
+
+# Define temp
+temp <- c(35, 88, 42, 84, 81, 30)
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+names(temp) <- city
+
+# Access the temperatures of Paris and San Juan
+temp[c(3,5)]
+
+# Create a vector m of integers that starts at 32 and ends at 99.
+m <- 32:99
+
+# Determine the length of object m.
+length(m)
+
+# Create a vector x of integers that starts at 12 and ends at 73.
+x <- 12:73
+# Determine the length of object x.
+length(x)
+# Create a vector with the multiples of 7, smaller than 50.
+seq(7, 49, 7) 
+
+# Create a vector containing all the positive odd numbers smaller than 100.
+# The numbers should be in ascending order
+seq(1,99,2)
+
+# We can create a vector with the multiples of 7, smaller than 50 like this 
+seq(7, 49, 7) 
+
+# But note that the second argument does not need to be the last number
+# It simply determines the maximum value permitted
+# so the following line of code produces the same vector as seq(7, 49, 7)
+seq(7, 50, 7)
+
+# Create a sequence of numbers from 6 to 55, with 4/7 increments and determine its length
+length(seq(6,55,4/7))
+
+# Store the sequence in the object a
+a <- seq(1, 10, length.out = 100)
+
+# Determine the class of a
+class(a)
+a
+
+
+# Store the sequence in the object a
+a <- seq(1, 10)
+
+# Determine the class of a
+class(a)
+
+# Check the class of 1, assigned to the object a
+class(1)
+
+# Confirm the class of 1L is integer
+class(1L)
+
+# Define the vector x
+x <- c(1, 3, 5,"a")
+
+# Note that the x is character vector
+x
+
+# Typecast the vector to get an integer vector
+# You will get a warning but that is ok
+x <- as.numeric(x)
+
+
+# Access the `state` variable and store it in an object 
+states <- murders$state 
+
+# Sort the object alphabetically and redefine the object 
+states <- sort(states) 
+
+# Report the first alphabetical value  
+states[1]
+
+# Access population values from the dataset and store it in pop
+pop <- murders$population
+# Sort the object and save it in the same object 
+pop <- sort(pop)
+# Report the smallest population size 
+pop[1]
+
+# Access population from the dataset and store it in pop
+pop <- murders$population
+# Use the command order to find the vector of indexes that order pop and store in object ord
+ord <- order(pop)
+# Find the index number of the entry with the smallest population size
+ord[1]
+# Find the index of the smallest value for variable total 
+which.min(murders$total)
+
+# Find the index of the smallest value for population
+which.min(murders$population)
+
+# Define the variable i to be the index of the smallest state
+i <- which.min(murders$population)
+
+# Define variable states to hold the states
+states <- murders$state
+# Use the index you just defined to find the state with the smallest population
+states[i]
+
+# Store temperatures in an object 
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Store city names in an object 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Create data frame with city names and temperature 
+city_temps <- data.frame(name = city, temperature = temp)
+
+# Define a variable states to be the state names 
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <- rank(murders$population)
+
+# Create a data frame my_df with the state name and its rank
+my_df <- data.frame(state = states, rank = ranks)
+my_df
+
+# Define a variable states to be the state names from the murders data frame
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <-rank(murders$population)
+
+# Define a variable ind to store the indexes needed to order the population values
+ind <- order(murders$population)
+
+# Create a data frame my_df with the state name and its rank and ordered from least populous to most 
+my_df <- data.frame(state <- states[ind], rank<-ranks[ind] )
+my_df
+
+# Using new dataset 
+library(dslabs)
+data(na_example)
+
+# Checking the structure 
+str(na_example)
+
+# Find out the mean of the entire dataset 
+mean(na_example)
+
+# Use is.na to create a logical index ind that tells which entries are NA
+ind <- is.na(na_example)
+# Determine how many NA ind has using the sum function
+sum(ind)
+
+# Note what we can do with the ! operator
+x <- c(1, 2, 3)
+ind <- c(FALSE, TRUE, FALSE)
+x[!ind]
+
+# Create the ind vector
+library(dslabs)
+data(na_example)
+ind <- is.na(na_example)
+
+# We saw that this gives an NA
+mean(na_example)
+
+# Compute the average, for entries of na_example that are not NA 
+mean(na_example[!ind])
+
+# Assign city names to `city` 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Store temperature values in `temp`
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Convert temperature into Celsius and overwrite the original values of 'temp' with these Celsius values
+temp <- 5/9*(temp - 32)
+# Create a data frame `city_temps` 
+city_temps <- data.frame(name = city, temperature = temp)
+
+# Define an object `x` with the numbers 1 through 100
+x <- 1:100
+# Compute the sum 
+sum(1/x**2)
+
+# Load the data
+library(dslabs)
+data(murders)
+
+# Store the per 100,000 murder rate for each state in murder_rate
+murder_rate <- murders$total / murders$population * 100000
+# Calculate the average murder rate in the US 
+mean(murder_rate)
+
+# Here is how you compute the sum for the first 20 integers
+20*(20+1)/2 
+
+# However, we can define a variable to use the formula for other values of n
+n <- 20
+n*(n+1)/2
+
+n <- 25
+n*(n+1)/2
+
+# Below, write code to calculate the sum of the first 100 integers
+n <- 100
+n*(n+1)/2
+
+# Below, write code to calculate the sum of the first 1000 integers 
+n<-1000
+n*(n+1)/2
+
+# log to the base 2 
+log2(16)
+
+# sqrt of the log to the base 2 of 16:
+sqrt(log2(16))
+
+# Compute log to the base 10 (log10) of the sqrt of 100. Do not use variables.
+log10(sqrt(100))
+
+# Load package and data
+
+library(dslabs)
+data(murders)
+
+# Use the function names to extract the variable names 
+names(murders)
+# To access the population variable from the murders dataset use this code:
+p <- murders$population 
+
+# To determine the class of object `p` we use this code:
+class(p)
+
+# Use the accessor to extract state abbreviations and assign it to a
+a<-murders$abb
+# Determine the class of a
+class(a)
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]] 
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+b <- murders[["abb"]]
+# Check if `a` and `b` are identical 
+identical(a, b)
+# We can see the class of the region variable using class
+class(murders$region)
+
+# Determine the number of regions included in this variable 
+length(levels(murders$region))
+# Here is an example of what the table function does
+x <- c("a", "a", "b", "b", "b", "c")
+table(x)
+
+# Write one line of code to show the number of states per region
+table(murders$region)
+
+# Here is an example creating a numeric vector named cost
+cost <- c(50, 75, 90, 100, 150)
+
+# Create a numeric vector to store the temperatures listed in the instructions into a vector named temp
+# Make sure to follow the same order in the instructions
+temp <- c(Beijing=35, Lagos= 88, Paris= 42, "Rio de Janeiro"= 84, "San Juan"= 81, Toronto=30)
+temp
+
+# here is an example of how to create a character vector
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")
+
+# Create a character vector called city to store the city names
+# Make sure to follow the same order as in the instructions
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Associate the cost values with its corresponding food item
+cost <- c(50, 75, 90, 100, 150)
+food <- c("pizza", "burgers", "salads", "cheese", "pasta")
+names(cost) <- food
+
+# You already wrote this code
+temp <- c(35, 88, 42, 84, 81, 30)
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Associate the temperature values with its corresponding city
+names(temp) <-city
+
+# cost of the last 3 items in our food list:
+cost[3:5]
+
+# temperatures of the first three cities in the list:
+temp[1:3]
+
+# Access the cost of pizza and pasta from our food list 
+cost[c(1,5)]
+
+# Define temp
+temp <- c(35, 88, 42, 84, 81, 30)
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+names(temp) <- city
+
+# Access the temperatures of Paris and San Juan
+temp[c(3,5)]
+
+# Create a vector m of integers that starts at 32 and ends at 99.
+m <- 32:99
+
+# Determine the length of object m.
+length(m)
+
+# Create a vector x of integers that starts at 12 and ends at 73.
+x <- 12:73
+# Determine the length of object x.
+length(x)
+# Create a vector with the multiples of 7, smaller than 50.
+seq(7, 49, 7) 
+
+# Create a vector containing all the positive odd numbers smaller than 100.
+# The numbers should be in ascending order
+seq(1,99,2)
+
+# We can create a vector with the multiples of 7, smaller than 50 like this 
+seq(7, 49, 7) 
+
+# But note that the second argument does not need to be the last number
+# It simply determines the maximum value permitted
+# so the following line of code produces the same vector as seq(7, 49, 7)
+seq(7, 50, 7)
+
+# Create a sequence of numbers from 6 to 55, with 4/7 increments and determine its length
+length(seq(6,55,4/7))
+
+# Store the sequence in the object a
+a <- seq(1, 10, length.out = 100)
+
+# Determine the class of a
+class(a)
+a
+
+
+# Store the sequence in the object a
+a <- seq(1, 10)
+
+# Determine the class of a
+class(a)
+
+# Check the class of 1, assigned to the object a
+class(1)
+
+# Confirm the class of 1L is integer
+class(1L)
+
+# Define the vector x
+x <- c(1, 3, 5,"a")
+
+# Note that the x is character vector
+x
+
+# Typecast the vector to get an integer vector
+# You will get a warning but that is ok
+x <- as.numeric(x)
+
+
+# Access the `state` variable and store it in an object 
+states <- murders$state 
+
+# Sort the object alphabetically and redefine the object 
+states <- sort(states) 
+
+# Report the first alphabetical value  
+states[1]
+
+# Access population values from the dataset and store it in pop
+pop <- murders$population
+# Sort the object and save it in the same object 
+pop <- sort(pop)
+# Report the smallest population size 
+pop[1]
+
+# Access population from the dataset and store it in pop
+pop <- murders$population
+# Use the command order to find the vector of indexes that order pop and store in object ord
+ord <- order(pop)
+# Find the index number of the entry with the smallest population size
+ord[1]
+# Find the index of the smallest value for variable total 
+which.min(murders$total)
+
+# Find the index of the smallest value for population
+which.min(murders$population)
+
+# Define the variable i to be the index of the smallest state
+i <- which.min(murders$population)
+
+# Define variable states to hold the states
+states <- murders$state
+# Use the index you just defined to find the state with the smallest population
+states[i]
+
+# Store temperatures in an object 
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Store city names in an object 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Create data frame with city names and temperature 
+city_temps <- data.frame(name = city, temperature = temp)
+
+# Define a variable states to be the state names 
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <- rank(murders$population)
+
+# Create a data frame my_df with the state name and its rank
+my_df <- data.frame(state = states, rank = ranks)
+my_df
+
+# Define a variable states to be the state names from the murders data frame
+states <- murders$state
+
+# Define a variable ranks to determine the population size ranks 
+ranks <-rank(murders$population)
+
+# Define a variable ind to store the indexes needed to order the population values
+ind <- order(murders$population)
+
+# Create a data frame my_df with the state name and its rank and ordered from least populous to most 
+my_df <- data.frame(state <- states[ind], rank<-ranks[ind] )
+my_df
+
+# Using new dataset 
+library(dslabs)
+data(na_example)
+
+# Checking the structure 
+str(na_example)
+
+# Find out the mean of the entire dataset 
+mean(na_example)
+
+# Use is.na to create a logical index ind that tells which entries are NA
+ind <- is.na(na_example)
+# Determine how many NA ind has using the sum function
+sum(ind)
+
+# Note what we can do with the ! operator
+x <- c(1, 2, 3)
+ind <- c(FALSE, TRUE, FALSE)
+x[!ind]
+
+# Create the ind vector
+library(dslabs)
+data(na_example)
+ind <- is.na(na_example)
+
+# We saw that this gives an NA
+mean(na_example)
+
+# Compute the average, for entries of na_example that are not NA 
+mean(na_example[!ind])
+
+# Assign city names to `city` 
+city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
+
+# Store temperature values in `temp`
+temp <- c(35, 88, 42, 84, 81, 30)
+
+# Convert temperature into Celsius and overwrite the original values of 'temp' with these Celsius values
+temp <- 5/9*(temp - 32)
+# Create a data frame `city_temps` 
+city_temps <- data.frame(name = city, temperature = temp)
+
+# Define an object `x` with the numbers 1 through 100
+x <- 1:100
+# Compute the sum 
+sum(1/x**2)
+
+# Load the data
+library(dslabs)
+data(murders)
+
+# Store the per 100,000 murder rate for each state in murder_rate
+murder_rate <- murders$total / murders$population * 100000
+# Calculate the average murder rate in the US 
+mean(murder_rate)
+
+
+# Store the 3 abbreviations in a vector called `abbs` (remember that they are character vectors and need quotes)
+abbs=c("AK","MI","IA")
+# Match the abbs to the murders$abb and store in ind
+ind <- match(abbs, murders$abb,  nomatch = NA_integer_, incomparables = NULL)
+ind
+# Print state names from ind
+murders$state[ind]
+
+# Store the 5 abbreviations in `abbs`. (remember that they are character vectors)
+abbs=c("MA","ME","MI","MO","MU")
+
+# Use the %in% command to check if the entries of abbs are abbreviations in the the murders data frame
+abbs %in% murders$abb
+
