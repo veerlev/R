10fea945d369c65d356c22ea69e50549a1c84d6f
veerlev <47982314+veerlev@users.noreply.github.com>
2019-09-14 10:52:18 +0100

    Update hXMooc.R

--- hXMooc.R
+++ hXMooc.R
@@ -751,3 +751,32 @@ data(murders)
 
 ## Define the rate column
 my_states <- murders %>% mutate(rate =  total / population * 100000, rank = rank(-rate)) %>% filter((region == "Northeast" | region == "West") & rate <1) %>% select(state, rate, rank)
+
+
+# Load the datasets and define some variables
+library(dslabs)
+data(murders)
+
+population_in_millions <- murders$population/10^6
+total_gun_murders <- murders$total
+
+plot(population_in_millions, total_gun_murders)
+
+# Transform population using the log10 transformation and save to object log10_population
+log10_population <- log10(murders$population)
+
+# Transform total gun murders using log10 transformation and save to object log10_total_gun_murders
+log10_total_gun_murders <- log10(murders$total)
+# Create a scatterplot with the log scale transformed population and murders 
+plot(log10_population, log10_total_gun_murders)
+
+# Store the population in millions and save to population_in_millions 
+population_in_millions <- murders$population/10^6
+
+
+# Create a histogram of this variable
+hist(population_in_millions)
+
+# Create a boxplot of state populations by region for the murders dataset
+boxplot(murders$population~murders$region)
+
