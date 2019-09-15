0a5dc5fc0582210d56a506047cf3f372cc751de8
veerlev <47982314+veerlev@users.noreply.github.com>
2019-09-15 14:41:48 +0100

    Add last assessment

--- hXMooc.R
+++ hXMooc.R
@@ -780,3 +780,64 @@ hist(population_in_millions)
 # Create a boxplot of state populations by region for the murders dataset
 boxplot(population~region, data=murders)
 
+(new_names <- ifelse(nchar(murders$state) > 8, murders$abb, murders$state)) 
+# Create function called `sum_n`
+sum_n <- function(x){
+  y <- x *(x + 1) / 2
+  y
+}
+# Use the function to determine the sum of integers from 1 to 5000
+(sum_n(500))
+
+# Create `altman_plot` 
+altman_plot <- function(x, y){
+  plot(x+y, y-x)
+}
+
+# Write a function compute_s_n with argument n that for any given n computes the sum of 1 + 2^2 + ...+ n^2
+compute_s_n <- function(n){
+  x <- 1:n
+  sum(x^2)
+}
+
+# Define a function and store it in `compute_s_n`
+compute_s_n <- function(n){
+  x <- 1:n
+  sum(x^2)
+}
+
+# Create a vector for storing results
+s_n <- vector("numeric", 25)
+
+# write a for-loop to store the results in s_n
+for (i in 1:25){
+  s_n[i] <- compute_s_n(i)
+}
+s_n
+
+# Define the function
+compute_s_n <- function(n){
+  x <- 1:n
+  sum(x^2)
+}
+
+# Define the vector of n
+n <- 1:25
+
+# Define the vector to store data
+s_n <- vector("numeric", 25)
+for(i in n){
+  s_n[i] <- compute_s_n(i)
+}
+
+#  Create the plot 
+plot(n, s_n)
+
+f_n <- vector("numeric", 25)
+for(i in n){
+  s_n[i] <- compute_s_n(i)
+  f_n[i] <- i * (i + 1)*(2 * i + 1)/6
+}
+# Check that s_n is identical to the formula given in the instructions.
+
+identical(s_n, f_n)
